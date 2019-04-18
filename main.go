6ea554dd4f61f8246ab626667fb1c675b27a3b10
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-04-18 14:41:38 +0200

    Create main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(HelloWorld())
+}
+
+// HelloWorld is a function that returns a string containing "hello world"
+func HelloWorld() string {
+	return "hello, world"
+}
